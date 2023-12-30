418831f4ff8c72ca27fe4e47a8000421f20c2200
Abhinav Sunil <abhinavsunil@Abhinavs-MacBook-Pro.local>
2023-12-29 21:01:19 -0800

    First commit

---
+++ igbot.py
@@ -0,0 +1,46 @@
+from instagrapi import Client
+from openai import OpenAI
+# from openai import OpenAI
+import json
+
+name = 'beauty.json'
+output = 'questions.json'
+
+questions = []
+
+#open json file of instagram posts with the hashtag beauty from apify
+
+with open(name, 'r') as file:
+    datas = json.load(file)
+
+#print(data[0]['caption'])
+    
+#create gpt client
+
+client = OpenAI(api_key="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")
+
+# function to make chatgpt generate a question based on a prompt
+
+def chat_with_gpt(prompt):
+    prefix = 'generate an interesting question based on this caption '
+    caption = prefix + prompt
+    response = client.chat.completions.create(model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": caption}])
+
+    return response.choices[0].message.content.strip()
+
+# loop through instagram posts and call chat_with_gpt based on the caption of the post
+
+for data in datas:
+    questions.append(chat_with_gpt(data['caption']))
+
+# with open(output, 'w') as json_file:
+#     json.dump(questions, json_file)
+    
+# output result to json file
+    
+with open(output, 'w') as json_file:
+    for question in questions:
+        json_file.write(json.dumps(question) + '\n')
+
+
